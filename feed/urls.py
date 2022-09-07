2a5e216bb1a3ac9dcbe173e7b773a52d4fa5836e
Liz Skert <eys.1908@gmail.com>
2022-09-07 16:18:20 -0400

    added ChangePasswordView

--- feed/urls.py
+++ feed/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.HomePage.as_view(), name="index"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
     path("new/", views.CreateNewPost.as_view(), name="new_post"),
+    path("password-change/", views.ChangePasswordView.as_view(), name="password_change"),
 ]
