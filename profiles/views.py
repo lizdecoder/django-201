4fa34ed8791b8aab34f8b716e75907683acbbc9b
Liz Skert <eys.1908@gmail.com>
2022-09-07 16:18:57 -0400

    updated files for profile updating

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,12 @@
 from http.client import HTTPResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse, HttpResponseBadRequest
+from .form import UpdateProfileForm, UpdateUserForm
 
 from feed.models import Post
 from followers.models import Follower
@@ -65,3 +69,28 @@ class FollowView(LoginRequiredMixin, View):
 			'success': True,
 			'wording': "Unfollow" if data['action'] == 'follow' else "Follow"
 		})
+
+# class UpdateProfile(FormView):
+#     template_name = "profiles/update_profile.html"
+#     form_class = UpdateProfileForm
+#     success_url = "/"
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile is updated successfully")
+            return redirect(to='/')
+
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, '/', {'user_form': user_form, 'profile_form': profile_form})
+
+
