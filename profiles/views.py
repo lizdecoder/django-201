568c7a9ab24ad0c195fd5d01a9fac1ca171d0d29
Liz Skert <eys.1908@gmail.com>
2022-09-08 17:11:45 -0400

    uploading userprofile.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,8 @@
 from http.client import HTTPResponse
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.views.generic import DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponseBadRequest
-from .form import UpdateProfileForm, UpdateUserForm
+from django.views.generic import DetailView, View
 
 from feed.models import Post
 from followers.models import Follower
@@ -70,27 +66,15 @@ class FollowView(LoginRequiredMixin, View):
 			'wording': "Unfollow" if data['action'] == 'follow' else "Follow"
 		})
 
-# class UpdateProfile(FormView):
-#     template_name = "profiles/update_profile.html"
-#     form_class = UpdateProfileForm
-#     success_url = "/"
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+class UserView(DetailView):
+	http_method_names = ['get']
+	template_name = 'profiles/userprofile.html'
+	model = User
+	context_object_name = "user"
+	slug_field = "username"
+	slug_url_kwarg = "username"
 
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, "Your profile is updated successfully")
-            return redirect(to='/')
-
-    else:
-        user_form = UpdateUserForm(instance=request.user)
-        profile_form = UpdateProfileForm(instance=request.user.profile)
-
-    return render(request, '/', {'user_form': user_form, 'profile_form': profile_form})
-
-
+	def get_context_data(self, **kwargs):
+		user = self.get_object()
+		context = super().get_context_data(**kwargs)
