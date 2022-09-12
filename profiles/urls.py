62a3e6aa6e9e1c99da8b06fee81d0696558cb7bd
Liz Skert <eys.1908@gmail.com>
2022-09-12 16:19:12 -0400

    change path for updated function view

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,5 +7,5 @@ app_name = "profiles"
 urlpatterns = [
 	path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
 	path("<str:username>/follow/", views.FollowView.as_view(), name="follow"),
-	path("update/<pk>/", views.UserUpdateView.as_view(), name="update")
+	path("profiles/<str:username>/update/", views.update_user, name="update"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,18 @@ from http.client import HTTPResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponseBadRequest
-from django.views.generic import DetailView, View
+from django.views.generic import DetailView, View, FormView
 from django.views.generic.edit import UpdateView
 
 from feed.models import Post
 from followers.models import Follower
+from profiles.models import Profile
+
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import UpdateImageForm, UserForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 class ProfileDetailView(DetailView):
 	http_method_names = ['get']
@@ -67,16 +74,47 @@ class FollowView(LoginRequiredMixin, View):
 			'wording': "Unfollow" if data['action'] == 'follow' else "Follow"
 		})
 
-class UserUpdateView(UpdateView):
-	model = User
-	fields = ['username', 'first_name', 'last_name']
-	template_name = 'profiles/updateprofile_form.html'
-	success_url = '/'
+# class UserUpdateView(UpdateView):
+# 	model = User
+# 	fields = ['username', 'first_name', 'last_name']
+# 	template_name = 'profiles/updateprofile_form.html'
+# 	success_url = '/'
 
-	def get_object(self, queryset=None):
-		self.object = User.objects.get(username=self.request.user.username)
-		return self.request.user
+# 	def get_object(self, queryset=None):
+# 		self.object = User.objects.get(username=self.request.user.username)
+# 		return self.request.user
 	
-	def form_valid(self, form):
-		self.object = form.save()
-		return super().form_valid(form)
+# 	def form_valid(self, form):
+# 		self.object = form.save()
+# 		return super().form_valid(form)
+
+@login_required
+def update_user(request, username):
+
+	try:
+		user_profile = Profile.objects.get(user=request.user)
+		user_info = User.objects.get(username=request.user)
+	except Profile.DoesNotExist:
+		return HTTPResponse("invalid user!")
+
+	if request.method == "POST":
+		update_user_form = UserForm(data=request.POST, instance=request.user)
+		update_image_form = UpdateImageForm(data=request.POST, instance=user_profile)
+
+		if update_user_form.is_valid() and update_image_form.is_valid():
+			user = update_user_form.save()
+			profile = update_image_form.save(commit=False)
+			profile.user = user
+
+			if 'image' in request.FILES:
+				profile.image = request.FILES['image']
+			
+			profile.save()
+
+		else:
+			print(update_user_form.errors, update_image_form.errors)
+	else:
+		update_user_form = UserForm(instance=user_info)
+		update_image_form = UpdateImageForm(instance=user_profile)
+	
+	return render(request, 'profiles/updateprofile_form.html', {'update_user_form': update_user_form, 'update_image_form': update_image_form})
